3084583eaa3ff630ac3a5db2c1567c5019c5a69b
plusbeauxjours <plusbeauxjours@gmail.com>
2020-05-23 13:42:50 +0700

    reportList done

--- src/screens/ReportListScreen/ReportListScreen.tsx
+++ src/screens/ReportListScreen/ReportListScreen.tsx
@@ -7,24 +7,60 @@ import { ActivityIndicator, RefreshControl, Platform } from "react-native";
 import { GetReportList, GetReportListVariables } from "../../types/api";
 import { GET_REPORT_LIST } from "./ReportListScreenQueries";
 import MenuCustomHeader from "../../components/MenuCustomHeader";
-import { Ionicons } from "@expo/vector-icons";
+import Moment from "moment";
 
 const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
 `;
-const IconContainer = styled.View`
-  margin: 20px 0;
-`;
 const View = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
 `;
-const Text = styled.Text``;
-const ScrollView = styled.ScrollView``;
+const Text = styled.Text`
+  font-weight: 400;
+  font-size: 20px;
+`;
+const SmallText = styled.Text`
+  font-weight: 400;
+  font-size: 12px;
+`;
+const ScrollView = styled.ScrollView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
 const Touchable = styled.TouchableOpacity``;
+const Row = styled.View`
+  width: 240px;
+  height: 80px;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+`;
+const FirstRow = styled(Row)`
+  width: 240px;
+  height: 80px;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+`;
+const GreyLine = styled.View`
+  width: 260px;
+  border-bottom-width: 1px;
+  border-bottom-color: #999;
+`;
+const Circle = styled.Text`
+  text-align: center;
+  margin-top: 20px;
+  font-size: 8px;
+  color: #999;
+  margin-bottom: 50px;
+`;
 
 const ReportListScreen: NavigationStackScreenComponent = ({ navigation }) => {
   const { me, loading, refetch } = useMe();
@@ -67,70 +103,64 @@ const ReportListScreen: NavigationStackScreenComponent = ({ navigation }) => {
         >
           <View>
             {me && me.user.hasPaid ? (
-              <Touchable
-                onPress={() =>
-                  navigation.navigate("CreateReportScreen", {
-                    reportCoverUuid: me.user.reportCoverUuid,
-                    reports: reports,
-                  })
-                }
-              >
-                {Platform.OS === "ios" ? (
-                  <IconContainer>
-                    <Ionicons
-                      name="ios-add-circle-outline"
-                      size={32}
-                      color="black"
-                    />
-                  </IconContainer>
-                ) : (
-                  <IconContainer>
-                    <Ionicons
-                      name="md-add-circle-outline"
-                      size={32}
-                      color="black"
-                    />
-                  </IconContainer>
-                )}
-              </Touchable>
+              <>
+                <Touchable
+                  onPress={() =>
+                    navigation.navigate("CreateReportScreen", {
+                      reportCoverUuid: me.user.reportCoverUuid,
+                      reports: reports,
+                    })
+                  }
+                >
+                  <FirstRow>
+                    <Text>새일지 제출하기</Text>
+                  </FirstRow>
+                </Touchable>
+                <GreyLine />
+              </>
             ) : (
-              <Touchable
-                onPress={() => navigation.navigate("CreateReportScreen")}
-              >
-                {Platform.OS === "ios" ? (
-                  <IconContainer>
-                    <Ionicons
-                      name="ios-add-circle-outline"
-                      size={32}
-                      color="black"
-                    />
-                  </IconContainer>
-                ) : (
-                  <IconContainer>
-                    <Ionicons
-                      name="md-add-circle-outline"
-                      size={32}
-                      color="black"
-                    />
-                  </IconContainer>
-                )}
-              </Touchable>
+              <>
+                <Touchable
+                  onPress={() => navigation.navigate("CreateReportScreen")}
+                >
+                  <FirstRow>
+                    <Text>새일지 제출하기</Text>
+                  </FirstRow>
+                </Touchable>
+                <GreyLine />
+              </>
             )}
             {reports &&
               reports.length !== 0 &&
               reports.map((report: any) => (
-                <Touchable
-                  key={report.uuid}
-                  onPress={() =>
-                    navigation.navigate("ReportDetailScreen", {
-                      reportUuid: report.uuid,
-                    })
-                  }
-                >
-                  <Text>{report.reportDate}</Text>
-                </Touchable>
+                <>
+                  <Touchable
+                    key={report.uuid}
+                    onPress={() =>
+                      navigation.navigate("ReportDetailScreen", {
+                        reportUuid: report.uuid,
+                      })
+                    }
+                  >
+                    <Row>
+                      <Text>
+                        {report.reportCover.classOrder &&
+                          Moment(report.reportDate).diff(
+                            Moment(report.reportCover.classOrder.startDate),
+                            "day"
+                          )}
+                        일차 일지
+                      </Text>
+                      <SmallText>
+                        ({Moment(report.reportDate).format("MM월 DD일")})
+                      </SmallText>
+                    </Row>
+                  </Touchable>
+                  <GreyLine />
+                </>
               ))}
           </View>
+          <Circle>●</Circle>
         </ScrollView>
       </>
     );
